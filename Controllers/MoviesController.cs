7b036b5f2d74c084f36cfc70c7384226a88f5501
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-04 17:12:34 +0530

    My First Page

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        //index is a action
+        //we can change it to random
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { 
+                Name= "3 Idiots"
+            };
+
+            return View(movie);
+        }
+    }
+}
